90f484dee69fe3006d937c6e483b842dbf21c979
NamasteDev.com <akshaysaini.in@gmail.com>
2021-01-04 11:12:54 +0530

    Added email validation

--- js/index.js
+++ js/index.js
@@ -5,10 +5,15 @@ If you're interested in working with us, please reach out to me at akshaysaini.i
 Cheers,
 NamasteDev Team 🙏`);
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 $(".thank-you").hide();
 submitBtn.addEventListener("click", function(){
   let email = nme.value;
+  if(!validateEmail(email)) return;
   var link='https://docs.google.com/forms/d/e/1FAIpQLScP-knMdl8gJw-S-qgoPvNlikq7Qn_qnEatIGjpkoXWpBi7lg/formResponse';
   jQuery.ajax({
         url: link,
